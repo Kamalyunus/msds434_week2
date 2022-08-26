94980a21062578d779f6c7df3d189ff41106545c
Kamalyunus <yunus.kamal@gmail.com>
2022-08-26 15:42:39 -0500

    final fix

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, send_file
 import pandas as pd
 from prophet import Prophet
-import os
 from google.cloud.sql.connector import Connector
 import sqlalchemy
 import pg8000
@@ -23,7 +22,7 @@ def getconn()->pg8000.dbapi.Connection:
 
 pool = sqlalchemy.create_engine(
     "postgresql+pg8000://",
-    creator=getconn(),
+    creator=getconn,
     pool_size=5,
     max_overflow=2,
     pool_timeout=30,  
@@ -36,20 +35,19 @@ pool = sqlalchemy.create_engine(
 def predict():
     zipcode = request.args.get('zipcode')
     horizon = request.args.get('horizon', type=int)
-    with pool.connect as db_conn:
-        results = db_conn.execute("SELECT * FROM covid_weekly limit 5").fetchall()
+    with pool.connect() as db_conn:
+        query="SELECT * FROM covid_weekly WHERE zip_code='"+zipcode+"'"
+        df = pd.read_sql_query(query, db_conn)
 
-    df = pd.read_csv('COVID19_Cases.csv')
-    df_zipcode = df[df['ZIP Code']==zipcode].rename(columns = {'Week Start': 'ds','Cases - Weekly': 'y'})
- 
+    df_zipcode = df.rename(columns = {'week_start': 'ds','cases_weekly': 'y'})
+    df_zipcode['ds'] = df_zipcode['ds'].dt.date
     model = Prophet(interval_width=0.95)
     model.fit(df_zipcode) 
     future_dates = model.make_future_dataframe(periods = horizon, freq="W", include_history=False)
     forecast = model.predict(future_dates)
     fig=model.plot(forecast, xlabel="Weeks", ylabel="Weekly Covid Cases for Zipcode: " + zipcode)
     fig.savefig('prophetplot.svg')
-    #return send_file('prophetplot.svg')
-    return results
+    return send_file('prophetplot.svg')
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
